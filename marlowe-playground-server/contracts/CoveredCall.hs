9a6bd4a67e8e9cf56817ec6df0d96b85c924341c
Yves Hauser <yves.hauser@gmail.com>
2021-12-08 11:20:44 +0100

    SCP-3083: Added premium payment to the contract

--- marlowe-playground-server/contracts/CoveredCall.hs
+++ marlowe-playground-server/contracts/CoveredCall.hs
@@ -13,6 +13,7 @@ contract =
     (Role "Counterparty")
     ada
     (Token "" "Underlying")
+    (ConstantParam "Premium")
     (ConstantParam "Strike")
     (ConstantParam "Ratio")
     (SlotParam "Initial Deposit Timeout")
@@ -24,40 +25,25 @@ coveredCall ::
   -> Party    -- ^ Counterparty, long the call option
   -> Token    -- ^ Currency
   -> Token    -- ^ Underlying
+  -> Value    -- ^ Premium (in currency)
   -> Value    -- ^ Strike price (in currency)
   -> Value    -- ^ Amount of underlying tokens per contract
   -> Timeout  -- ^ Timeout for initial deposit
   -> Timeout  -- ^ Expiration date (American style exercise)
   -> Contract -- ^ Covered Call contract
-coveredCall party counterparty currency underlying strike ratio timeout expiry =
-  deposit timeout party underlying ratio                  -- Party deposits an underlying into the contract
-    $ exercise
-        counterparty                                      -- Counterparty chooses to exercise the call option
-        Close                                             -- No exercise, the party is refunded
-        ( deposit expiry counterparty currency strike     -- Exercise, the Counterparty has to deposit the strike
-        $ Pay counterparty (Party party) currency strike  -- Strike is payed to the Party
-        $ Pay party (Party counterparty) underlying ratio -- Underlying is payed to the Counterparty
-          Close
-        )
-        expiry                                            -- Expiry date for the call option
+coveredCall party counterparty currency underlying premium strike ratio timeout expiry =
+    deposit timeout party underlying ratio                -- Party deposits an underlying into the contract
+  $ transfer timeout counterparty party currency premium  -- Counterparty pays the premium into the contract
+  $ exercise
+      counterparty                                        -- Counterparty chooses to exercise the call option
+      Close                                               -- No exercise, the party is refunded
+      ( deposit expiry counterparty currency strike       -- Exercise, the Counterparty has to deposit the strike
+      $ Pay counterparty (Party party) currency strike    -- Strike is payed to the Party
+      $ Pay party (Party counterparty) underlying ratio   -- Underlying is payed to the Counterparty
         Close
-
--- |Building block for `Deposit`
-deposit ::
-     Timeout  -- ^ Timeout for deposit
-  -> Party    -- ^ Deposit into account of party
-  -> Token    -- ^ Token to deposit
-  -> Value    -- ^ Amount of token
-  -> Contract -- ^ Continuation after deposit
-  -> Contract -- ^ Contract
-deposit timeout from token amount cont =
-  When
-    [ Case
-        (Deposit from from token amount)
-        cont
-    ]
-    timeout
-    Close
+      )
+      expiry                                              -- Expiry date for the call option
+      Close
 
 -- |Building block for option exercise contract logic
 exercise ::
@@ -76,3 +62,25 @@ exercise party contract0 contract1 =
   where
     choiceId :: ChoiceId
     choiceId = ChoiceId "exercise" party
+
+-- |Building block for `Deposit`
+deposit :: Timeout -> Party -> Token -> Value -> Contract -> Contract
+deposit timeout from token amount cont =
+  When
+    [ Case
+        (Deposit from from token amount)
+        cont
+    ]
+    timeout
+    Close
+
+-- |Building block for `Pay`
+pay :: Party -> Party -> Token -> Value -> Contract -> Contract
+pay from to = Pay from (Party to)
+
+-- |Building block for `Deposit` and `Pay`
+transfer :: Timeout -> Party -> Party -> Token -> Value -> Contract -> Contract
+transfer timeout from to token amount continuation =
+    deposit timeout from token amount
+  $ pay from to token amount
+    continuation
